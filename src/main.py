76541e3ce6fbc388fdc844d8d6b415da18f28d42
Carlo Ledesma <carlo.miguel.ledesma@gmail.com>
2015-04-16 17:03:36 +0800

    Updated region

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ class Main:
 	def __init__(self):
 		self.aws_access_key_id = os.environ['INFRA_1_ID']
 		self.aws_secret_access_key = os.environ['INFRA_1_SECRET']
-		self.aws_region = "us-west-2"
+		self.aws_region = os.environ['INFRA_1_REGION']
 
 	def connect(self):
 		conn = boto.ec2.connect_to_region(self.aws_region, aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key)
